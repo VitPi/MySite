5a64c5ed2bdb102cd400385a99188991afe84653
ViTali <cpaaaik@gmail.com>
2021-12-14 15:04:42 +0300

    add: my contact

---
+++ button/js/index.js
@@ -0,0 +1,10 @@
+//
+
+
+$(".mail-btn").on("click touchstart", function () {
+    $(this).addClass("fly");
+    that = this
+    setTimeout(function () {
+        $(that).removeClass("fly");
+    }, 5400)
+});
