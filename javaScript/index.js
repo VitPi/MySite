aac922259c8c6c6941fc83f90df33ae7e745cf14
ViTali <cpaaaik@gmail.com>
2021-12-19 15:48:51 +0300

    add: adding anchor

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,10 +1,21 @@
+//
+//
 
 
+"use strict";
 
 
+let inputCheck = document.getElementById('menu-toggle'),
+    link = document.querySelectorAll('.menu-item');
+
+function hideInput() {
+    inputCheck.checked = false
+}
 
 
-
+for (const i of link) {
+    i.addEventListener('click', hideInput)
+}
 
 
 
